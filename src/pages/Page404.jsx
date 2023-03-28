eec7483bd02155a1a0ddff0596465efecd3cf48b
J-Patlmx <lamoureux.jp61@gmail.com>
2023-03-28 17:35:46 +0200

    add montserra font fix textdecoration

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -1,15 +1,17 @@
+/* eslint-disable react/jsx-no-undef */
 function Page404() {
     return (
 
       <div className="main">
         <h1 className="notfound">404</h1>
         <p className="pNotFound">Oups! La page que vous demandez n'existe pas.</p>
-
-        <a
+  
+       
+        {/* <NavLink className="backtoHome" to="/" end="Home">Retourner sur la page d’accueil</NavLink> */}
+       <a
           className="backtoHome"
           href="/Home"
-          
-          rel="noopener noreferrer"> Retourner sur la page d’accueil</a>
+          rel="noopener noreferrer"> Retourner sur la page d’accueil</a> 
       </div>
 
     );
