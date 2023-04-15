f73cb6b1cd49a1d7addfa776062bacb688900b62
J-Patlmx <lamoureux.jp61@gmail.com>
2023-04-15 09:46:55 +0200

    WIP commentary FIX margin home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import {NavLink} from "react-router-dom";
+import {NavLink} from "react-router-dom";// permet de créer des liens de navigation qui gèrent l'état de l'élément qui si attache
 import logo from '../assets/LOGO.png'
 function Header() {
     return (
@@ -6,7 +6,7 @@ function Header() {
         <header className="App-header top-bar">
                     <img src={logo} className="App-logo" alt="logo" />
           <nav className="Header-link">
-            
+         {/* Utiliser l'attribut "to" pour spécifier l'URL de destination */}
             <NavLink className="App-header-link" to={"/Home"} >Accueil</NavLink>
             <NavLink className="App-header-link" to={"/About"}>A propos</NavLink>
           </nav>
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,14 +1,20 @@
 import { useState } from 'react';
-
+// Définir le composant Rating
 const Rating = ({ note }) => {
+  
+  // Utiliser le hook useState pour initialiser l'état de filledStars à la note arrondie
   const [filledStars] = useState(Math.floor(note));
+
+    // Calculer le nombre d'étoiles restantes à remplir
   const remainingStars = 5 - filledStars;
 
   return (
     <div className="rating">
+      {/* Afficher les étoiles pleines avec une couleur personnalisée */}
       {[...Array(filledStars)].map((_, i) => (
         <i key={i} className="fas fa-star" style={{ color: "#ff6060" }}></i>
       ))}
+        {/* Afficher les étoiles vides */}
       {[...Array(remainingStars)].map((_, i) => (
         <i key={i + filledStars} className="far fa-star"></i>
       ))}
